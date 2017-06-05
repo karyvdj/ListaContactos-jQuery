572ab74bfc2066ae1c3e67fd9f0fcca655d33930
karyvdj <kaina51@hotmail.com>
2017-06-05 09:34:38 -0500

    Ajustando contenedor de contacto ingresado

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,10 +5,12 @@ $(document).ready(function(){
     var $obtenerTelefono = $("#telefono").val();
 
     // creamos elementos
-    var $contenedorContacto = $("<div/>", {"class":"row"})
-    var $nombreNuevo = $("<p/>", {"class":"col-xs-8 col-xs-offset-1"});
-    var $telefonoNuevo = $("<p/>", {"class":"col-xs-8 col-xs-offset-1"});
-    var $botonBorrar = $("<button class='borrar btn'><span class='glyphicon glyphicon-trash'></span></button>");
+    var $contenedorContacto = $("<article/>", {"class":"container jumbotron marginBottom"})
+    var $row = $("<div/>", {"class":"row"})
+    var $nombreNuevo = $("<h3/>", {"class":"col-xs-4 col-xs-offset-1"});
+    var $row2 = $("<div/>", {"class":"row"})
+    var $telefonoNuevo = $("<h5/>", {"class":"col-xs-4 col-xs-offset-1"});
+    var $botonBorrar = $("<button class='borrar btn col-xs-offset-5'><span class='glyphicon glyphicon-trash'></span></button>");
 
     $botonBorrar.click(function(){
       $(this).parent().remove();
@@ -20,9 +22,12 @@ $(document).ready(function(){
 
 
     // agregamos los elementos
-    $contenedorContacto.append($nombreNuevo);
-    $contenedorContacto.append($telefonoNuevo);
-    $contenedorContacto.append($botonBorrar);
+    $row.append($nombreNuevo);
+    $row2.append($telefonoNuevo);
+    $row2.append($botonBorrar);
+
+    $contenedorContacto.prepend($row2);
+    $contenedorContacto.prepend($row);
 
     $nuevoContacto.prepend($contenedorContacto);
 
@@ -30,10 +35,7 @@ $(document).ready(function(){
     $("#telefono").val("");
     $("#myModal").modal("hide");
 
-		$("#agregar").attr("disabled");
-
-
-
+		$(this).attr("disabled", true);
   });
 
   $("#cancelar").click(function(){
